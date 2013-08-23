ccc651d7dc656177c78cf7571318ff4d661b561a
twneale <twneale@gmail.com>
2013-08-22 22:46:59 -0400

    garbage

--- commit-garbage.sh
+++ commit-garbage.sh
@@ -1,3 +1,5 @@
-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32000 | head -n 1) > ~/projects/imsoproductive/fakefile.py
+cd ~/projects/imsoproductive
+$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32000 | head -n 1) > fakefile.py
+git add .
 git commit fakefile.py
 git push origin master
