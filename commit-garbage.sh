dbd1da152982aefeca1d3a13d5e3164f1caeb2ca
twneale <twneale@gmail.com>
2013-08-22 22:48:42 -0400

    fake

--- commit-garbage.sh
+++ commit-garbage.sh
@@ -1,5 +1,5 @@
 cd ~/projects/imsoproductive
-$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32000 | head -n 1) > fakefile.py
+echo $(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32000 | head -n 1) > fakefile.py
 git add .
-git commit fakefile.py
+git commit fakefile.py -m"Another fake commit"
 git push origin master
