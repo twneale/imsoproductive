14c48f7c1b18d8a17b62dd4adf0c26834dbdf332
Thom Neale <twneale@gmail.com>
2013-09-02 16:11:21 -0400

    silly

--- schedule-commits.sh
+++ schedule-commits.sh
@@ -3,5 +3,5 @@ for number in $(seq 30)
 do
     hour=$(( ${RANDOM}*24/32768 ))
     minute=$(( ${RANDOM}*60/32768 ))
-    at -f ~/projects/commit-garbage.sh $(printf "%02d" ${hour}):$(printf "%02d" ${minute})
+    at -f ~/projects/imsoproductive/commit-garbage.sh $(printf "%02d" ${hour}):$(printf "%02d" ${minute})
 done
